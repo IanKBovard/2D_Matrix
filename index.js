4184155e4bd3a911dd2703848d572b0f958ccec5
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-19 11:20:51 -1000

    fixed initial function

--- index.js
+++ index.js
@@ -3,16 +3,31 @@
  function gridInit(){
   let grid = [];
   function gridMaker (rows, columns){
-    for (let i = 0; i <= rows; i++){
+    for (let i = 0; i < rows; i++){
       grid.push([]);
       for (let j = 1; j <= columns; j++){
-        grid[i].push(j);
+        grid[i].push([]);
       }
     }
     return grid;
   }
- return gridMaker;
+  function gridAppend (array, rows, columns){
+    for (let i = array.length - rows; i <= rows - array.length; i++){
+      grid.push([]);
+      for (let j = array.length - columns; j <= columns - array.length; j++){
+        grid[i].push(j);
+        console.log(grid);
+      }
+    }
+    return grid;
+  }
+ return {
+  gridMaker : gridMaker,
+  gridAppend : gridAppend
+};
  }
 let grid = gridInit();
-let newGrid = grid(4,3);
-console.log(newGrid);
+console.log(grid);
+let firstGrid = grid.gridMaker(4,3);
+console.log(firstGrid);
+
